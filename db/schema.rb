0011e892956dacc7abec822b9e4c9acf231481e5
Eric Euresti <eeuresti@gmail.com>
2015-10-28 15:18:27 -0700

    didn't change things

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151027215735) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
@@ -41,6 +44,6 @@ ActiveRecord::Schema.define(version: 20151027215735) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
